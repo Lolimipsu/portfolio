fc8963af3e1789f407a81a3854120dfffcf9f5fd
Borgar <44487404+Lolimipsu@users.noreply.github.com>
2024-12-03 16:22:17 +0800

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,20 @@
+// variables for the name and image
+let name_new = "Caseum Borgaro";
+let imageSrc = "./assets/png/caseum_borgaro.png";
+
+// Function to update the content of the span and the image
+// follows top down approach, so it starts from the very top, meaning the website title.
+function updateContent(name, imageSrc) {
+    document.title = `${name} - Portfolio`;
+    // The picture
+    document.getElementById('logo-img').src = imageSrc;
+    document.getElementById('logo-img').alt = `${name} Logo Image`;
+    // The name beside the picture
+    document.getElementById('logo-sub').textContent = name;
+    // The big name at on-load
+    document.getElementById('home-name-primary').textContent = `My name is ${name}`
+    // The Get to know me section
+    document.getElementById('about-name').innerHTML = `Hi, I'm <strong>${name}</strong>.`;
+}
+
+updateContent(name_new, imageSrc);
