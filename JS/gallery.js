baac17e2c139abb7710a3a390077c2cf264e72cc
Borgar <44487404+Lolimipsu@users.noreply.github.com>
2024-12-12 09:53:55 +0800

    added automatic slideshow

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,4 +1,6 @@
 var slideIndex = 1;
+var slideInterval;
+
 showDivs(slideIndex);
 
 function plusDivs(n) {
@@ -9,18 +11,40 @@ function currentDiv(n) {
   showDivs(slideIndex = n);
 }
 
+function startSlideshow() {
+  slideInterval = setInterval(function() {
+    plusDivs(1);
+  }, 2000, );
+}
+
+function stopSlideshow() {
+  clearInterval(slideInterval);
+}
+
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = x.length
+  }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" w3-white", "");
   }
-  x[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " w3-white";
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-white";
 }
+
+window.onload = function() {
+  showDivs(slideIndex);
+  startSlideshow();
+  var slideshowContainer = document.querySelector('.w3-content');
+  slideshowContainer.onmouseover = stopSlideshow;
+  slideshowContainer.onmouseout = startSlideshow;
+};
